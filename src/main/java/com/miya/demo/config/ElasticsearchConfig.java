8771b6eb6d5c70e4a743554088b84b8c3cb9aa94
Cai,Xiaowei <v_XiaoweiCai@budweiserapac.com>
2021-12-07 17:56:47 +0800

    整合elasticsearch: 入门2

--- src/main/java/com/miya/demo/config/ElasticsearchConfig.java
+++ src/main/java/com/miya/demo/config/ElasticsearchConfig.java
@@ -1,7 +1,9 @@
 package com.miya.demo.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +16,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
 
+	public static final RequestOptions COMMON_OPTIONS;
+
+	static {
+		// RequestOptions类保存了请求的部分，这些部分应该在同一个应用程序中的许多请求之间共享。
+		// 创建一个singqleton实例，并在所有请求之间共享它。可以设置请求头之类的一些配置
+		RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//		builder.addHeader("Authorization", "Bearer " + TOKEN);
+//		builder.setHttpAsyncResponseConsumerFactory(
+//		new HttpAsyncResponseConsumerFactory
+//		     .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 *1024));
+		COMMON_OPTIONS = builder.build();
+	}
+
 	@Bean
-	public RestClient restClient() {
-		RestClient restClient = RestClient.builder(
-				new HttpHost("localhost", 9200, "http")
-		).build();
+	public RestHighLevelClient restClient() {
+		RestHighLevelClient restClient = new RestHighLevelClient (
+				RestClient.builder(
+						new HttpHost("localhost", 9200, "http")
+				)
+		);
 		return restClient;
 	}
 
--- src/test/java/com/miya/demo/Demo1ApplicationTests.java
+++ src/test/java/com/miya/demo/Demo1ApplicationTests.java
@@ -4,15 +4,18 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.json.JSONUtil;
 import com.google.common.collect.Lists;
 import com.miya.demo.common.util.MinioUtil;
+import com.miya.demo.config.ElasticsearchConfig;
 import com.miya.demo.entity.Customer;
 import com.miya.demo.service.CustomerService;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.errors.*;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.client.Request;
-import org.elasticsearch.client.Response;
-import org.elasticsearch.client.RestClient;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -80,12 +83,27 @@ class Demo1ApplicationTests {
 
 
 	@Autowired
-	private RestClient restClient;
+	private RestHighLevelClient restClient;
 
 	@Test
-	public void testElastic() throws IOException {
-		Request request = new Request("GET", "/");
-		Response response = restClient.performRequest(request);
+	public void testElastic0() throws IOException {
+		IndexRequest request = new IndexRequest("posts");
+		request.id("1");
+
+		String jsonString = "{" +
+				"\"user\":\"kimchy\"," +
+				"\"postDate\":\"2013-01-30\"," +
+				"\"message\":\"trying out Elasticsearch\"" +
+				"}";
+		IndexRequest source = request.source(jsonString, XContentType.JSON);
+
+		log.info("source---> {}", JSONUtil.toJsonStr(source));
+	}
+
+	@Test
+	public void testElastic1() throws IOException {
+		GetRequest request = new GetRequest("posts", "1");
+		GetResponse response = restClient.get(request, ElasticsearchConfig.COMMON_OPTIONS);
 		log.info("response---> {}", JSONUtil.toJsonStr(response));
 	}
 }
